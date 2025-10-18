3ad5d7034015f1a1d0147b707d60a47a8504f3fc
Sunny Sen <sensunny@users.noreply.github.com>
2025-10-18 17:13:21 +0530

    Added google-services.json

---
+++ config/firebase.ts
@@ -0,0 +1,20 @@
+export const firebaseConfig = {
+  projectId: 'oncare-7b874',
+  messagingSenderId: '105818631334802150884',
+  appId: '1:105818631334802150884:android:your-app-id',
+  apiKey: 'your-api-key',
+};
+
+export const firebaseServiceAccount = {
+  type: 'service_account',
+  project_id: 'oncare-7b874',
+  private_key_id: '4260262854ee6c63f70637d86176e798cb7cc758',
+  private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDvDC1FLPT1eOXw\nBd9sBIyVVra88dZ58TiGB1a4cc6ju1UJlyuJgoJwuwBNKET7kq4v13CcPGYLfkJu\ndrb6VQ/1aM3aDwP1LfkOtz9dVM+g56ip8ff6W0WgIyi0ak7Jz7J07uw62SUS3B7N\nNjMfsmtNImtUPf38KklebcTgEGsp71MzLOfvuHjunA80pTdoipY0SzJe453UHsML\nYhucQashRmiRKdZJzbkPOj3SHBaE0+HQOAVzpJD2RbqtSzwA/CyGqVLJ11UsVk9n\nUyFlDIZUgKTheeCJKZYFCjv1m9JZjp8uy66zKa9AfbrvZTpWzjVQ8UamZnl9Dn+c\nTnzfjdBFAgMBAAECggEAAp8k0PUnA5ktlmYOFvpuy0V98BEL697yIoAqmo++9ys7\nCcWjZ9jrUs6uOGpoZUTszaAEf2YnLVmXY2xAYfr0V5nsZgeToMj+CgQG+SBQvaBE\nyIZblJ6TIyg3jlD7J7n0KFWleBjBEBWjbdc6FM8pX5lwyWX7ptwU3WkBR37OmhFd\nL8ahB8KKwGYYtNrYistKGeDhnGPyEJYdvlNUtKgHw9HHc+S/9tsjxbGKDBwXq0vj\nbXAG5gwUzGKW9/tvfkRzO0KB3PAQ1HPyo0h5Q7TSpxyXOkyp94bbnT1WyvKCC3S8\n+dwvxlOfmC9CR5/cJxy/O7BBQdhoiBvzoud3vNfAIQKBgQD+2Sr/AHXfse1jgG28\nWtc5Q2rFDjNNxWqbb2P/o0oYFR1ifRwYieHoXUGK9p8eQ4G+9tbuyFLXXzsNWn2A\njRDVZ9Lbnwm20ruIDLBluQxvrZ9tipdyeO+vcIYoSgKcQ6BLCExAqYwcpS6L9Fxv\nuEwJYmuPCALQLQp2L7A85V+lYQKBgQDwILqntI4DqkKI0PWJQCjCes+wzWskU2F+\nco+YXu1F/UY43FRQdKRyGfPN9msmmOtM2cBsZEUL95rNLOe9366c8d/zC9doNHav\nDU5kWFt/TSBQ7xkvk3Ma/fXGGbopAlRlkWGNYEqF8RxDacEw2djdr3vaT1lJftvK\nbs6jmGoxZQKBgQDA1ib4qaqHx6SvI+DlI3+zrtW9og8kC/us/c6XA0HvkxqXCcAW\nuU6deuM7eX3Rcpdcnn7oU+CLY13VruFEQnm4YUVmNaMuGasZOTDI6AsG/kOiqboL\nPoNvJx/8oHP1Nh62Z+L7EtAYZM0hzZecWcH/X9+vbQpj55EA+PjMO2vyQQKBgQDC\nQ/g+DRiHZosFXOgH7dwL2oeEJev0K3DId64sO0ACS3EwNzAPFw6jMNPJYP4276gN\nOG0wQ76Ie/zE2Pty5S/C0Pu5CW9vYmJSJJWzILiFOMRYNddOYh/RSnw3lEDDRJs6\nwG8HjUs9KV8QOtKMzvFgB8RPTbqSdwshrpuGRpxC8QKBgQCq8lsJhYY4EcYDQlm3\nxlJCoupim+iMMHdHKb9aZbMZeE0OiMFvSIzZDSMrwv7YV7eUKdK0nTP6Il8igxMJ\nxh8J9kejhQR0E9hbF2g4A4jb7MFYg7KQg7DraoqvlT4UN53HcpRGAzNaxgzKCzeW\nkakpJpdEa6CxIR4dRL8UCbnRVw==\n-----END PRIVATE KEY-----\n',
+  client_email: 'firebase-adminsdk-fbsvc@oncare-7b874.iam.gserviceaccount.com',
+  client_id: '105818631334802150884',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40oncare-7b874.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com',
+};
